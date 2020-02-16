d55b90d7f1e4d1b46d471df7d188b454f64bf040
JIAWEI LYU <ljw960926@gmail.com>
2020-02-16 15:50:24 -0600

    update startup file

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 wget --no-check-certificate https://modelrelease.blob.core.windows.net/mass/zhen_mass_pre-training.pt -P ./MASS/model
 
+python -m nltk.downloader all
+
 gunicorn --config ./conf/gunicorn_config.py src:app
